7f62a403b96b610f2cd01c5c123fe99a4498c37d
Vince Abuyuan <rpabuyuan@ucsb.edu>
2023-11-08 21:16:55 -0800

    api dependencies, boilerplate

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,24 +1,28 @@
 // src/app.ts
-import express, { Request, Response } from 'express';
-import axios from 'axios';
+import express, { Request, Response, Application } from "express";
+import axios from "axios";
 
-const app = express();
+const app: Application = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with Axios!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Express with Axios!");
 });
 
-app.get('/fetch-data', async (req: Request, res: Response) => {
+app.get("/fetch-data", async (req: Request, res: Response) => {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
     res.json(response.data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An error occurred while fetching data.' });
+    res.status(500).json({ error: "An error occurred while fetching data." });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
